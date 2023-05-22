cb70602983730e898b0b6b90e01345fe3f5964b9
HenryWilder <henrythepony@gmail.com>
2023-05-22 14:51:27 -0400

    Got multiple selection editing working

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const selectionStr = (selection: vscode.Selection) => {
+	return `[[${selection.start.character},${selection.start.line}]-[${selection.end.character},${selection.end.line}]]`;
+};
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "henryrefactors" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('henryrefactors.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+		const editor = vscode.window.activeTextEditor;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Henry Refactors!');
+		if (editor) {
+			const document = editor.document;
+			const selections = editor.selections;
+			editor.edit((editBuilder) => {
+				for (const range of selections) {
+					const word = document.getText(range);
+					const reversed = word.split('').reverse().join('');
+					console.log(`Selection ${selectionStr(range)}\n    Range text: "${word}"\n    Reversed text: "${reversed}".\n  It is now ${document.getText(range)}.`);
+					editBuilder.replace(range, reversed);
+				}
+			});
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
