227d8037e6a46eca5d82789febe58c300d8be7af
HenryWilder <henrythepony@gmail.com>
2023-05-23 10:34:50 -0400

    Improved descriptiveness of cssPreview documentation

--- src/hw/cssPreview.ts
+++ src/hw/cssPreview.ts
@@ -4,8 +4,9 @@ import * as utils from './utils';
 /**
  * Displays a small preview of a generic HTML element with the selected CSS applied.
  * 
- * The HTML used is generated from the selectors.
+ * The HTML used is generated from the selectors, and text content is produced from lorem ipsum.
+ * Both of these can be configured through options given in annotating comments.
  */
 export function cssPreview(): void {
-
+    
 }
