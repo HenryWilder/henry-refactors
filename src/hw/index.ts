b20953a49d50b135fdea29fe75cef30ce5a3acc6
HenryWilder <henrythepony@gmail.com>
2023-05-23 11:55:46 -0400

    Fixed error

--- src/hw/index.ts
+++ src/hw/index.ts
@@ -1,9 +1,9 @@
 import { cmdHenryAlign } from './henryAlign';
-import { cssPreview } from './cssPreview';
+import { cmdCssPreview } from './cssPreview';
 import * as utils from './utils';
 
 /** The list of commands to register for the extension. */
 export const hwCommands: utils.TypeOLambda[] = [
     utils.editorCommand(cmdHenryAlign),
-    utils.editorCommand(cssPreview),
+    utils.editorCommand(cmdCssPreview),
 ];
